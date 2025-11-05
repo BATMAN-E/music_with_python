1729a749d387157433cb6912fd167b73b055b470
Eusha Mohammad Khan <eushamdkhan123@gmail.com>
2025-11-05 22:49:28 +0600

    Update geminifun.py

--- geminifun.py
+++ geminifun.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-API_KEY = "AIzaSyA6HP2yuMpSEpcRGRSihZgKuQ64D8kUewE"
+API_KEY = 
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-2.0-flash")
@@ -13,4 +13,5 @@ while True:
     if user_input.lower() == 'exit':
         break
     response = chat.send_message(user_input)
+
     print("Gemini:" , response.text)
